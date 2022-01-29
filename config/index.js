e6d2e6082c6346a673b86f61ea52c23d7fe679bb
Chris Nunes <chrisnunes57@gmail.com>
2022-01-28 19:35:41 -0800

    updated api url fr fr

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://crossword-search-engine.vercel.app/';
+export const server = dev ? 'http://localhost:3000' : 'https://crossword-search-engine-iufqescw3-chrisnunes57.vercel.app';
