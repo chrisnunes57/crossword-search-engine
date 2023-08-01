f565972478210613a1ee00b89323a15ba7ed47ac
Chris Nunes <chrisnunes57@gmail.com>
2023-07-31 18:20:29 -0700

    added log

--- pages/api/query.js
+++ pages/api/query.js
@@ -14,6 +14,7 @@ const validateQuery = (req) => {
 export default async function handler(req, res) {
 
     if (!index) {
+        console.log("getting index")
         index = await getReverseIndex();
     }
 
